901dae1e6e1ba3bd3721c3e9141e2a301b644d69
AlegreEmanuel <127903124+AlegreEmanuel@users.noreply.github.com>
2023-05-09 20:43:54 -0300

    Update py.py

--- py.py
+++ py.py
@@ -3,10 +3,54 @@ from datetime import datetime
 from typing import List
 import os
 
+from rich.console import Console
+from rich.table import Table
 
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def mostrar_tarea(tarea):
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("ID")
+    table.add_column("Título")
+    table.add_column("Descripción")
+    table.add_column("Estado")
+    table.add_column("Creada")
+    table.add_column("Actualizada")
+
+    table.add_row(
+        str(tarea.id),
+        tarea.titulo,
+        tarea.descripcion,
+        tarea.estado,
+        str(tarea.creada),
+        str(tarea.actualizada),
+        )
+
+    console.print(table)
+
+def mostrar_tareas(tareas):
+    console = Console()
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("ID")
+    table.add_column("Título")
+    table.add_column("Descripción")
+    table.add_column("Estado")
+    table.add_column("Creada")
+    table.add_column("Actualizada")
+
+    for tarea in tareas:
+        table.add_row(
+            str(tarea.id),
+            tarea.titulo,
+            tarea.descripcion,
+            tarea.estado,
+            str(tarea.creada),
+            str(tarea.actualizada),
+        )
+
+    console.print(table)
 
 class Tarea:
     def __init__(self, id, titulo, descripcion, estado, creada, actualizada):
@@ -21,36 +65,18 @@ class Tarea:
 class AdminTarea:
     def __init__(self, db):
         self.db = TinyDB(db)
-        
-    def actualizar_ids_tareas(self):
-        tareas = self.tareas.all()
-        tareas_ordenadas = sorted(tareas, key=lambda tarea: int(tarea['id']) if 'id' in tarea else 0)
-        ids_existente = set(map(lambda tarea: tarea['id'], tareas))
-        
-        for i, tarea in enumerate(tareas_ordenadas):
-            tarea_id_anterior = tarea['id']
-            nueva_tarea = tarea.copy()
-            nueva_tarea['id'] = i + 1
-
-            while nueva_tarea['id'] in ids_existente:
-                nueva_tarea['id'] += 1
-
-            self.tareas.update(nueva_tarea, Query().id == tarea_id_anterior)
-            ids_existente.add(nueva_tarea['id'])
+        self.tareas = self.db.table('tareas')
 
     def get_last_id(self) -> int:
-        last_tarea = self.tareas.get(doc_id=len(self.tareas))
-        if last_tarea is not None:
+        tareas = self.tareas.all()
+        if tareas:
+            last_tarea = tareas[-1]
             return last_tarea['id']
         else:
             return 0
 
     def agregar_tarea(self, tarea: Tarea) -> int:
         last_id = self.get_last_id()
-        if last_id == 0:
-            estado = 'en proceso'
-        else:
-            estado = 'en espera'
         tarea_dict = {
             'id': last_id + 1,
             'titulo': tarea.titulo,
@@ -86,16 +112,10 @@ class AdminTarea:
         tarea = self.tareas.get(Query().id == tarea_id)
         if tarea is not None:
             self.tareas.remove(doc_ids=[tarea.doc_id])
-            for tareaPosterior in self.tareas.search(Query().id > tarea_id):
-                self.tareas.update({'id': tareaPosterior['id'] - 1},
-                                   doc_ids=[tareaPosterior.doc_id])
-        else:
-            print("No se encontró la tarea con ID", tarea_id)
 
     def eliminar_todas_las_tareas(self):
         self.tareas.truncate()
-        print("Se han eliminado todas las tareas")
-
+        
     
     def traer_todas_tareas(self) -> List[Tarea]:
         tareas_dicts = self.tareas.all()
@@ -114,12 +134,12 @@ if __name__ == '__main__':
 
 
     while True:
+        print("\n")
         print('Ingrese una opción:')
         print('1. Agregar tarea')
         print('2. Ver tarea')
         print('3. Eliminar tarea')
-        print('4. Ver todas las tareas')
-        print('5. Salir')
+        print('4. Salir')
         print("\n")
 
         opcion = input('Opción: ')
@@ -127,7 +147,7 @@ if __name__ == '__main__':
         if opcion == "1":
             titulo = input("Ingrese el título de la tarea: ")
             descripcion = input("Ingrese la descripción de la tarea: ")
-            estado = "creando"
+            estado = input("Ingrese estado de la tarea: ")
             creada = str(datetime.now())
             actualizada = creada
             
@@ -139,26 +159,29 @@ if __name__ == '__main__':
         
         elif opcion == "2":
             clear_console()
-            while True:
-                try:
-                    tarea_id = int(input("Ingrese el ID de la tarea: "))
-                    break
-                except ValueError:
-                    print("Código no válido")
-                    
-            tarea = admin_tareas.traer_tarea(tarea_id)
-
-            if tarea:
-                print("ID:", tarea.id)
-                print("Título:", tarea.titulo)
-                print("Descripción:", tarea.descripcion)
-                print("Estado:", tarea.estado)
-                print("Creada:", tarea.creada)
-                print("Actualizada:", tarea.actualizada)
+            print("\n")
+            opcion = input("Ingrese 1 para ver una tarea particular o 2 para ver todas las tareas: ")
+            print("\n")
+            if opcion == '1':
+                tarea_id = int(input("Ingrese el ID de la tarea: "))
                 print("\n")
+                tarea = admin_tareas.traer_tarea(tarea_id)
+                if tarea:
+                    mostrar_tarea(tarea)
+                else:
+                    print("No se encontró la tarea con ID", tarea_id)
+                    print("\n")
+
+            elif opcion == '2':
+                tareas = admin_tareas.traer_todas_tareas()
+                if tareas:
+                        mostrar_tareas(tareas)
+                else:
+                    print("No hay tareas registradas.")
+                    print("\n")
             else:
-                print("No se encontró la tarea con ID", tarea_id)
-                
+                print("Opción inválida.")
+                print("\n")
         
         elif opcion == "3":
             clear_console()
@@ -167,30 +190,13 @@ if __name__ == '__main__':
                 tarea_id = int(input("Ingrese el ID de la tarea: "))
                 admin_tareas.eliminar_tarea(tarea_id)
                 print("Se ha eliminado la tarea con ID", tarea_id)
-                
             elif opcion == '2':
                 admin_tareas.eliminar_todas_las_tareas()
+                print("Se han eliminado todas las tareas")
             else:
                 print("Opción inválida.")
                
         elif opcion == "4":
-            clear_console()
-
-            tareas = admin_tareas.traer_todas_tareas()
-            
-            if tareas:
-                for tarea in tareas:
-                    print("ID:", tarea.id)
-                    print("Título:", tarea.titulo)
-                    print("Descripción:", tarea.descripcion)
-                    print("Estado:", tarea.estado)
-                    print("Creada:", tarea.creada)
-                    print("Actualizada:", tarea.actualizada)
-                    print("\n")
-            else:
-                print("No hay tareas registradas.")
-
-        elif opcion == "5":
                 break
 
         else:
